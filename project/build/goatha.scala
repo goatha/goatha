de13eb1c3968597ee47773e1984c4a8dcf1f5caa
unknown <y_yoshida@.dp-corp.net>
2009-12-15 14:35:13 +0900

    update lift 1.1-M8

--- project/build/goatha.scala
+++ project/build/goatha.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 class goatha(info: ProjectInfo) extends DefaultWebProject(info) {
-  val lift      = "net.liftweb"       % "lift-core"   % "1.1-M7"   % "compile"
+  val lift      = "net.liftweb"       % "lift-core"   % "1.1-M8"   % "compile"
   val jetty6    = "org.mortbay.jetty" % "jetty"       % "6.1.19"   % "test"
   val servlet   = "javax.servlet"     % "servlet-api" % "2.5"      % "provided"
   val derby     = "org.apache.derby"  % "derby"       % "10.4.2.0" % "runtime"
--- src/main/scala/com/goatha/snippet/HelloWorld.scala
+++ src/main/scala/com/goatha/snippet/HelloWorld.scala
@@ -4,15 +4,17 @@ import _root_.scala.xml.{NodeSeq, Text}
 import _root_.net.liftweb.util._
 import _root_.net.liftweb.common._
 import _root_.java.util.Date
-import goatha.lib._
+import com.goatha.lib._
 import Helpers._
+
+import _root_.java.lang.reflect._
 
 class HelloWorld {
   lazy val date: Box[Date] = DependencyFactory.inject[Date] // inject the date
 
   def howdy(in: NodeSeq): NodeSeq =
   Helpers.bind("b", in, "time" -> date.map(d => Text(d.toString)))
-   
+
   /*
    lazy val date: Date = DependencyFactory.time.vend // create the date via factory
 
